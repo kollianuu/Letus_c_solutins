6e95ba5e9db5b566116494e25e5a032ccac02c83
Kolli Anusha <120302424+kollianuu@users.noreply.github.com>
2023-12-30 09:40:38 +0530

    Update 1a.c

--- 1a.c
+++ 1a.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    float basic_salary, dallowance, house_rent, gross_salary;
+    float basic_salary, dear_allowance, house_rent, gross_salary;
     
     printf("Enter Basic Salary: ");
     scanf("%f", &basic_salary);
 
-    dallowance = 0.4 * basic_salary;
+    dear_allowance = 0.4 * basic_salary;
     house_rent = 0.2 * basic_salary;
 
     gross_salary = basic_salary + dallowance + house_rent;
 
+    printf("\n Dear allowance: %.2f", dear_allowance);
+    printf("\n House rent is: %.2f", house_rent);
     printf("\n Gross Salary: %.2f", gross_salary);
 }
